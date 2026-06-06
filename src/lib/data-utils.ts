52ee71805840e5155f694c317aa6355421e266f5
wuwenbin970731 <wuwenbin970731@gmail.com>
2026-06-07 00:50:31 +0800

    chore(public): 更新站点图标与PWA配置
    - 更新全站 favicon 系列图标资源
    - 修改默认头像路径为 /static/logo-wenbin.png
    - 更新PWA应用名称为 Wenbin Notes 与短名称 Wenbin

--- src/lib/data-utils.ts
+++ src/lib/data-utils.ts
@@ -202,7 +202,7 @@ export async function parseAuthors(authorIds: string[] = []) {
     return {
       id,
       name: author?.data?.name || id,
-      avatar: author?.data?.avatar || '/static/logo.png',
+      avatar: author?.data?.avatar || '/static/logo-wenbin.png',
       isRegistered: !!author,
     }
   })
